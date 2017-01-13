9b0b01d79ef74606f72c5a3eae73c7c41bd17463
anmolbabu <anmolbudugutta@gmail.com>
2017-01-13 10:32:51 +0530

    Incoporate comments
    tendrl-bug-id: Tendrl/alerting#10
    Signed-off-by: anmolbabu <anmolbudugutta@gmail.com>

--- tendrl/alerting/watcher/manager.py
+++ tendrl/alerting/watcher/manager.py
@@ -1,39 +1,38 @@
-import etcd
+import json
 import logging
 import multiprocessing
-from tendrl.alerting.exceptions import AlertingError
-from tendrl.common.alert import Alert
-from tendrl.common.config import ConfigNotFound
-from tendrl.common.config import TendrlConfig
+import Queue
+from tendrl.commons.alert import AlertUtils
 
-config = TendrlConfig()
 LOG = logging.getLogger(__name__)
 
 
 class AlertsWatchManager(multiprocessing.Process):
-    def __init__(self, alert_queue):
+    def __init__(self, alert_queue, etcd_client):
         super(AlertsWatchManager, self).__init__()
         self.alert_queue = alert_queue
-        try:
-            etcd_kwargs = {
-                'port': int(config.get("common", "etcd_port")),
-                'host': config.get("common", "etcd_connection")
-            }
-            self.etcd_client = etcd.Client(**etcd_kwargs)
-        except ConfigNotFound as ex:
-            LOG.error(
-                'Intialization of alerts watcher failed. Error %s' % str(ex),
-                exc_info=True
-            )
-            raise AlertingError(str(ex))
+        self.etcd_client = etcd_client
 
     def run(self):
-        while True:
-            new_alert = self.etcd_client.watch(
-                '/alerts',
-                recursive=True,
-                timeout=0
+        try:
+            while True:
+                new_alert = self.etcd_client.watch(
+                    '/alerts',
+                    recursive=True,
+                    timeout=0
+                )
+                if new_alert is not None:
+                    if new_alert.value is not None:
+                        self.alert_queue.put(
+                            AlertUtils().to_obj(
+                                json.loads(new_alert.value)
+                            )
+                        )
+        except Queue.Full as ex:
+            LOG.error(
+                'Exception in alert watcher.Error %s' % str(
+                    ex),
+                exc_info=True
             )
-            if new_alert is not None:
-                if new_alert.value is not None:
-                    self.alert_queue.put(Alert.to_obj(str(new_alert.value)))
+            raise ex
+
