c5f783cb59cd267f02348d47322ecf487c0852a4
root <root@dhcp43-146.lab.eng.blr.redhat.com>
2017-10-09 12:45:03 +0530

    Added configuration file for snmp notification channel
    tendrl-bug-id: Tendrl/notifier#13
    
    Signed-off-by: root <root@dhcp43-146.lab.eng.blr.redhat.com>

--- tendrl/notifier/notification/handlers/snmp_handler.py
+++ tendrl/notifier/notification/handlers/snmp_handler.py
@@ -9,9 +9,12 @@ from pysnmp.hlapi import OctetString
 from pysnmp.hlapi import sendNotification
 from pysnmp.hlapi import SnmpEngine
 from pysnmp.hlapi import UdpTransportTarget
+from pysnmp.hlapi import usmDESPrivProtocol
+from pysnmp.hlapi import usmHMACMD5AuthProtocol
+from pysnmp.hlapi import UsmUserData
+from tendrl.commons.config import load_config
 from tendrl.commons.event import Event
 from tendrl.commons.message import ExceptionMessage
-from tendrl.commons.utils import etcd_utils
 from tendrl.commons.utils.log_utils import log
 from tendrl.notifier.notification import NotificationPlugin
 
@@ -19,6 +22,8 @@ from tendrl.notifier.notification import NotificationPlugin
 ENGINE_ID = "8000000001020304"
 # default snmp port
 PORT = 162
+V2_ENDPOINT = "v2_endpoint"
+V3_ENDPOINT = "v3_endpoint"
 ALERT_TYPES = {0: "STATUS",
                1: "UTILIZATION"}
 
@@ -26,9 +31,9 @@ ALERT_TYPES = {0: "STATUS",
 class SnmpEndpoint(object):
     # Base class for SNMP endpoint (credential and host+port pair)
 
-    def __init__(self, host="localhost", proto=0):
+    def __init__(self, host_ip="127.0.0.1", proto=0):
         self.proto = proto
-        self.host = host
+        self.host = host_ip
         self.port = PORT
         self.engineid = ENGINE_ID
 
@@ -36,9 +41,22 @@ class SnmpEndpoint(object):
 class V2Endpoint(SnmpEndpoint):
     # Class encapsulating an SNMPv2c community, host+port pair
     # 2 represent SNMPv2
-    def __init__(self, host):
-        super(V2Endpoint, self).__init__(host, 2)
-        self.community = "public"
+    def __init__(self, host_ip, community):
+        super(V2Endpoint, self).__init__(host_ip, 2)
+        self.community = community
+
+
+class V3Endpoint(SnmpEndpoint):
+    # Class encapsulating an SNMPv2c community, host+port pair
+    # 2 represent SNMPv2
+    def __init__(self, host_ip, username, auth_key, priv_key):
+        super(V3Endpoint, self).__init__(host_ip, 3)
+        self.usm_user = UsmUserData(userName=username,
+                                    authKey=auth_key,
+                                    privKey=priv_key,
+                                    authProtocol=usmHMACMD5AuthProtocol,
+                                    privProtocol=usmDESPrivProtocol
+                                    )
 
 
 class SnmpHandler(NotificationPlugin):
@@ -49,8 +67,7 @@ class SnmpHandler(NotificationPlugin):
 
     def set_destinations(self):
         try:
-            key = "_tendrl/indexes/notifications/snmp_notifications"
-            self.user_configs = self.get_alert_destinations(key)
+            self.user_configs = self.get_alert_destinations()
         except (
             EtcdException,
             EtcdKeyNotFound,
@@ -63,15 +80,12 @@ class SnmpHandler(NotificationPlugin):
                 return
             raise ex
 
-    def get_alert_destinations(self, key):
-        hosts = []
-        snmp_notifications = etcd_utils.read(key)
-        for snmp_notification in snmp_notifications.leaves:
-            host = NS._int.wclient.read(
-                snmp_notification.key
-            ).value
-            hosts.append(host)
-        return hosts
+    def get_alert_destinations(self):
+        snmp_config = load_config(
+            'notifier',
+            '/etc/tendrl/notifier/snmp.conf.yaml'
+        )
+        return snmp_config
 
     def format_message(self, alert):
         resource = alert.resource.replace("_", " ").title()
@@ -103,19 +117,74 @@ class SnmpHandler(NotificationPlugin):
                 # sequence of custom OID-value pairs
                 self.getPDU(message)))
         if errorIndication:
-            print("result: {} {} {}".format(errorIndication, errorStatus,
-                                            errorIndex))
+            log(
+                "error",
+                "notifier",
+                {
+                    "message": 'Unable to sent snmp message to %s err:%s %s %s'
+                    % (endpoint.host,
+                       errorIndication,
+                       errorStatus,
+                       errorIndex)
+                }
+            )
+        else:
+            log(
+                "info",
+                "notifier",
+                {
+                    "message": 'Sent snmp message to %s to alert about %s'
+                    % (endpoint.host,
+                       message)
+                }
+            )
+
+    def trapV3(self, endpoint, message):
+        # Send trap to one endpoint
+        errorIndication, errorStatus, errorIndex, varBinds = next(
+            sendNotification(
+                SnmpEngine(snmpEngineID=OctetString(
+                    hexValue=endpoint.engineid)),
+                endpoint.usm_user,
+                UdpTransportTarget((endpoint.host, endpoint.port)),
+                ContextData(),
+                'trap',
+                # sequence of custom OID-value pairs
+                self.getPDU(message)))
+        if errorIndication:
+            log(
+                "error",
+                "notifier",
+                {
+                    "message": 'Unable to sent snmp message to %s err:%s %s %s'
+                    % (endpoint.host,
+                       errorIndication,
+                       errorStatus,
+                       errorIndex)
+                }
+            )
+        else:
+            log(
+                "info",
+                "notifier",
+                {
+                    "message": 'Sent snmp message to %s to alert about %s'
+                    % (endpoint.host,
+                       message)
+                }
+            )
 
     def send_message(self, endpoint, message):
         if endpoint.proto == 2:
             self.trapV2(endpoint, message)
+        elif endpoint.proto == 3:
+            self.trapV3(endpoint, message)
 
     def dispatch_notification(self, alert):
         try:
             self.set_destinations()
             if (
-                not self.user_configs or
-                len(self.user_configs) == 0
+                self.user_configs is None
             ):
                 log(
                     "error",
@@ -148,17 +217,19 @@ class SnmpHandler(NotificationPlugin):
             return ex
         try:
             msg = self.format_message(alert)
-            for user_config in self.user_configs:
-                v2_endpoint = V2Endpoint(user_config)
-                self.send_message(v2_endpoint, msg)
-                log(
-                    "debug",
-                    "notifier",
-                    {
-                        "message": 'Sent snmp message to %s to alert about %s'
-                        % (user_config, msg)
-                    }
-                )
+            for endpoint in self.user_configs:
+                if endpoint == V2_ENDPOINT:
+                    for user_config in self.user_configs[endpoint]:
+                        v2_endpoint = V2Endpoint(
+                            **self.user_configs[endpoint][user_config]
+                        )
+                        self.send_message(v2_endpoint, msg)
+                elif endpoint == V3_ENDPOINT:
+                    for user_config in self.user_configs[endpoint]:
+                        v3_endpoint = V3Endpoint(
+                            **self.user_configs[endpoint][user_config]
+                        )
+                        self.send_message(v3_endpoint, msg)
         except (
             PySnmpError,
             KeyError,
